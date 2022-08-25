699efab06f561efd0a27989fdab627385c208479
jojoc4 <me@jojoc4.ch>
2022-08-25 14:33:53 +0200

    dev env

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 require 'config.php';
 
 //https redirection
-if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' ||
-        $_SERVER['HTTPS'] == 1) ||
-        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
-    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $redirect);
-    exit();
+if($FORCE_HTTPS){
+    if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' ||
+            $_SERVER['HTTPS'] == 1) ||
+            isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+            $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
+        $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $redirect);
+        exit();
+    }
 }
 ?>
 
