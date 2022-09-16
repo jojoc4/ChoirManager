25d244e43e16218a4e80e2c2e5a6e77d955abddb
jojoc4 <me@jojoc4.ch>
2022-09-16 10:47:18 +0200

    home cards in db

--- home.php
+++ home.php
@@ -1,27 +1,8 @@
 <div class="container-fluid overflow-visible">
-
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h4>Accueil</h4>
-        </div>
-        <div class="card-body">
-            Bienvenue sur la plateforme privée Chœur à Cœur.<br><br>
-        </div>
-    </div>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h4>Absence</h4>
-        </div>
-        <div class="card-body">
-            Pour les choristes absents : s’annoncer uniquement auprès de Pascale (078 743 48 11)<br><br>
-        </div>
-    </div>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h4>Cotisations</h4>
-        </div>
-        <div class="card-body">
-            Le cotisations qui sont de 240.- par personne ou 200.- par personne pour les couples sont à verser sur le compte IBAN CH22 0079 0016 9385 6392 7.<br><br>
-        </div>
-    </div>
+    <?php
+        $cards = $pdo->query("SELECT * FROM homepage")->fetchAll(PDO::FETCH_ASSOC);
+        foreach($cards as $card){
+            echo $card['content'];
+        }
+    ?>    
 </div>
