5e335c8807875da145466074cf63b39d492f1380
jojoc4 <me@jojoc4.ch>
2022-09-16 10:49:44 +0200

    bdd update

--- db.sql
+++ db.sql
@@ -1,184 +1,82 @@
--- phpMyAdmin SQL Dump
--- version 4.9.6
--- https://www.phpmyadmin.net/
---
--- Hôte : plwe.myd.infomaniak.com
--- Généré le :  jeu. 25 août 2022 à 11:17
--- Version du serveur :  10.4.18-MariaDB-1:10.4.18+maria~stretch-log
--- Version de PHP :  7.4.30
+-- Adminer 4.8.1 MySQL 5.5.5-10.9.2-MariaDB-1:10.9.2+maria~ubu2204 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `config` (
+  `id_cfg` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id_cfg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Base de données :  `plwe_intranet`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `date`
---
+INSERT INTO `config` (`id_cfg`, `name`, `value`) VALUES
+(1,	'USER_PASSWORD',	'userpass'),
+(2,	'ADMIN_PASSWORD',	'adminpass'),
+(3,	'FORCE_HTTPS',	'false');
 
 CREATE TABLE `date` (
-  `id_date` int(10) UNSIGNED NOT NULL,
+  `id_date` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `comment` text NOT NULL,
+  PRIMARY KEY (`id_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `document`
---
 
 CREATE TABLE `document` (
-  `id_doc` int(10) UNSIGNED NOT NULL,
+  `id_doc` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_music` int(11) NOT NULL,
   `url` varchar(120) NOT NULL,
-  `name` varchar(30) NOT NULL DEFAULT 'Partition'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(30) NOT NULL DEFAULT 'Partition',
+  PRIMARY KEY (`id_doc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `folder`
---
 
 CREATE TABLE `folder` (
-  `id_fld` int(10) UNSIGNED NOT NULL,
+  `id_fld` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `icon` varchar(30) NOT NULL DEFAULT 'music',
   `header` text NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_fld`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `folder_music`
---
 
 CREATE TABLE `folder_music` (
-  `id_folder_music` int(10) UNSIGNED NOT NULL,
-  `id_music` int(10) UNSIGNED NOT NULL,
-  `id_fld` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id_folder_music` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_music` int(10) unsigned NOT NULL,
+  `id_fld` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_folder_music`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `music`
---
+CREATE TABLE `homepage` (
+  `id_hp` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` longtext NOT NULL,
+  PRIMARY KEY (`id_hp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE `music` (
-  `id_music` int(10) UNSIGNED NOT NULL,
-  `name` varchar(120) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id_music` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) COLLATE utf8mb3_bin NOT NULL,
+  `directory` varchar(120) COLLATE utf8mb3_bin NOT NULL,
+  PRIMARY KEY (`id_music`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `version`
---
 
 CREATE TABLE `version` (
-  `id_version` int(10) UNSIGNED NOT NULL,
+  `id_version` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_music` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `url` varchar(120) NOT NULL,
-  `number` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `number` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `date`
---
-ALTER TABLE `date`
-  ADD PRIMARY KEY (`id_date`);
-
---
--- Index pour la table `document`
---
-ALTER TABLE `document`
-  ADD PRIMARY KEY (`id_doc`);
-
---
--- Index pour la table `folder`
---
-ALTER TABLE `folder`
-  ADD PRIMARY KEY (`id_fld`);
-
---
--- Index pour la table `folder_music`
---
-ALTER TABLE `folder_music`
-  ADD PRIMARY KEY (`id_folder_music`) USING BTREE;
-
---
--- Index pour la table `music`
---
-ALTER TABLE `music`
-  ADD PRIMARY KEY (`id_music`);
-
---
--- Index pour la table `version`
---
-ALTER TABLE `version`
-  ADD PRIMARY KEY (`id_version`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `date`
---
-ALTER TABLE `date`
-  MODIFY `id_date` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `document`
---
-ALTER TABLE `document`
-  MODIFY `id_doc` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `folder`
---
-ALTER TABLE `folder`
-  MODIFY `id_fld` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `folder_music`
---
-ALTER TABLE `folder_music`
-  MODIFY `id_folder_music` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `music`
---
-ALTER TABLE `music`
-  MODIFY `id_music` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `version`
---
-ALTER TABLE `version`
-  MODIFY `id_version` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2022-09-16 08:48:54
